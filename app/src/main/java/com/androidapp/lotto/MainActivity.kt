d274ca04df831174b3425f357f3f948bece11d3f
C-H Han <itq1@naver.com>
2021-05-12 14:20:03 +0900

    Open ConstellationActivity

--- app/src/main/java/com/androidapp/lotto/MainActivity.kt
+++ app/src/main/java/com/androidapp/lotto/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.androidapp.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val constellationCard = findViewById<CardView>(R.id.constellationCard)
+
+        constellationCard.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ConstellationActivity::class.java))
+        }
     }
 }
