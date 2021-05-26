b9e953b1bfce62959b4fc0e9d11c2d7ade1a2fd6
C-H Han <itq1@naver.com>
2021-05-26 13:24:56 +0900

    Random number generation

--- app/src/main/java/com/androidapp/lotto/MainActivity.kt
+++ app/src/main/java/com/androidapp/lotto/MainActivity.kt
@@ -4,6 +4,38 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.cardview.widget.CardView
+import java.util.*
+
+fun getRandomLottoNumber (): Int{
+    return Random().nextInt(45)+1
+}
+
+fun getRandomLottoNumbers(): MutableList<Int>{
+    val lottoNumbers = mutableListOf<Int>()
+
+    while(true){ //중복방지지
+       var number: Int = getRandomLottoNumber()
+        var flag_existing: Int = 0
+        //for(j in 0..lottoNumbers.size) {
+            if(lottoNumbers.contains((number))){
+            //if (number.equals((lottoNumbers[j]))) {
+            flag_existing = 1
+            //break;
+                continue
+        }
+/*        if(flag_existing.equals(1))
+            continue
+        else*/
+        lottoNumbers.add(number)
+        if(lottoNumbers.size >= 6)
+            break;
+    }
+
+    /*for(i in 1..6){
+        lottoNumbers.add(getRandomLottoNumber())
+    }*/
+    return lottoNumbers
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +45,9 @@ class MainActivity : AppCompatActivity() {
         val RandomCard = findViewById<CardView>(R.id.RandomCard)
 
         RandomCard.setOnClickListener {
-            startActivity(Intent(this@MainActivity, ResultActivity::class.java))
+            val intent = Intent(this@MainActivity, ResultActivity::class.java)
+            intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
+            startActivity(intent)
         }
 
         val constellationCard = findViewById<CardView>(R.id.constellationCard)
@@ -27,7 +61,5 @@ class MainActivity : AppCompatActivity() {
         NameCard.setOnClickListener {
             startActivity(Intent(this@MainActivity, NameActivity::class.java))
         }
-
-
     }
 }
